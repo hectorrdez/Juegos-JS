237eb7066be4b2a558b50c337bea5db351a27bc3
11ife <11ife>
2022-02-28 12:17:04 +0100

    añadida funcion jugador

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+var dado1 = parseInt(Math.random() * 6) + 1;
+document.getElementById("img1").src = "/dados/" + dado1 + ".png"
+
+var dado2 = parseInt(Math.random() * 6) + 1;
+document.getElementById("img2").src = "/dados/" + dado2 + ".png"
+
+var dado3 = parseInt(Math.random() * 6) + 1;
+document.getElementById("img3").src = "/dados/" + dado3 + ".png"
+
+var dado4 = parseInt(Math.random() * 6) + 1;
+document.getElementById("img4").src = "/dados/" + dado4 + ".png"
+
+var dado5 = parseInt(Math.random() * 6) + 1;
+document.getElementById("img5").src = "/dados/" + dado5 + ".png"
