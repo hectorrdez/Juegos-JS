58af367919b1a5cf8beb8a47dbd647e794ed67ce
11ife <11ife>
2022-03-02 16:42:12 +0100

    Total jugadores y valor por ronda

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-let ronda = 0, sumaRonda, dado1, dado2, dado3, dado4, dado5;
-let player1, player2;
-player1 = prompt("Nombre Jugador 1:");
+let player1 = prompt("Jugador 1:");
 document.getElementById("p1").innerHTML = player1;
-player2 = prompt("Nombre Jugador 2:");
+let player2 = prompt("Jugador 2:");
 document.getElementById("p2").innerHTML = player2;
+
+let ronda = 0;
+let total1Score = 0;
+let total2Score = 0;
+
+
 function tirarDados() {
-    sumaRonda = 0;
-    dado1 = parseInt(Math.random() * 6) + 1;
-    document.getElementById("d1").innerHTML = dado1
-    document.getElementById("img1").src = "/dados/" + dado1 + ".png"
+    if (ronda % 2 == 0) document.getElementById("anuncios").innerHTML = "Te toca " + player1;
+    if (ronda % 2 != 0) document.getElementById("anuncios").innerHTML = "Te toca " + player2;
 
-    dado2 = parseInt(Math.random() * 6) + 1;
-    document.getElementById("d2").innerHTML = dado2
-    document.getElementById("img2").src = "/dados/" + dado2 + ".png"
+    let dado1 = parseInt(Math.random() * 6) + 1;
+    let dado2 = parseInt(Math.random() * 6) + 1;
+    let dado3 = parseInt(Math.random() * 6) + 1;
+    let dado4 = parseInt(Math.random() * 6) + 1;
 
-    dado3 = parseInt(Math.random() * 6) + 1;
-    document.getElementById("d3").innerHTML = dado3
-    document.getElementById("img3").src = "/dados/" + dado3 + ".png"
+    document.getElementById("img1").src = "/dados/" + dado1 + ".png";
+    document.getElementById("img2").src = "/dados/" + dado2 + ".png";
+    document.getElementById("img3").src = "/dados/" + dado3 + ".png";
+    document.getElementById("img4").src = "/dados/" + dado4 + ".png";
 
-    dado4 = parseInt(Math.random() * 6) + 1;
-    document.getElementById("d4").innerHTML = dado4
-    document.getElementById("img4").src = "/dados/" + dado4 + ".png"
-
-    dado5 = parseInt(Math.random() * 6) + 1;
-    document.getElementById("d5").innerHTML = dado5
-    document.getElementById("img5").src = "/dados/" + dado5 + ".png"
-}
-sumaRonda = dado1 + dado2 + dado3 + dado4 + dado5;
-    console.log(sumaRonda);
-    if (ronda % 2 == 0) {
-        alert("Te toca " + player1);
-
-    } else {
-        alert("Te toca " + play2);
+    ronda++;
+    let sumaRonda = dado1 + dado2 + dado3 + dado4;
+    if(ronda%2 != 0){
+        total1Score = total1Score + sumaRonda;
+        document.getElementById("total1").innerHTML = total1Score;
+    }
+    if(ronda%2 == 0){
+        total2Score = total2Score + sumaRonda;
+        document.getElementById("total2").innerHTML = total2Score;
     }
     document.getElementById("ronda" + ronda).innerHTML = sumaRonda;
-    ronda++;
+    sumaRonda = 0;
+    if(ronda > 12){
+        if(total1Score > total2Score){
+            document.getElementById("anuncios").innerHTML = `Ha ganado  + ${player1}`;
+        }else{
+            document.getElementById("anuncios").innerHTML = `Ha ganado ${player2}`;
+        }
+        ronda = 0;
+        total1Score = 0;
+        total2Score = 0;
+        console.log("MAXIMO DE RONDA");
+    }
+}
