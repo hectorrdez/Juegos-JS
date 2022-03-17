b0d80166f5c86342c777a36c6e99439e91c11957
Nicolas <nicoa88@gmail.com>
2022-03-17 20:14:22 +0100

    huikh

--- Tragaperras/game.js
+++ Tragaperras/game.js
@@ -12,10 +12,10 @@ function changeImg(){
     for(i=0;i<img.length;i++){
         index[i]++;
         if (index[i]>15) index[i]=0;
-        let player = img[i].animate([
-            {transform: 'translate(0, -100px)'},
-            {transform: 'translate(0, 250px)'}
-          ], 200);
+        // let player = img[i].animate([
+        //     {transform: 'translate(0, -100px)'},
+        //     {transform: 'translate(0, 250px)'}
+        //   ], 200);
           
         //let player = img[i].animate([{ transform: "translate(0px, 50px)" }], 150);
         img[i].src="img/"+index[i]+".png";
