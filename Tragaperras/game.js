267257dccbf0d9cf7a2680bae0c26321f4cc87c6
11ife <11ife>
2022-03-21 17:56:22 +0100

    Co-authored-by: NicoA_88@hotmail.com <NicoA_88@hotmail.com>

--- Tragaperras/game.js
+++ Tragaperras/game.js
@@ -38,7 +38,7 @@ for (let i = 0; i < moneyCount.length; i++) {
 }
 let display = [document.querySelectorAll('.display1 img'), document.querySelectorAll('.display2 img'), document.querySelectorAll('.display3 img')];
 let index = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-let timer0,timer1,timer2;
+let timer0,timer1,timer2,interval0,interval1,interval2;
 
 let tiradaBt = document.querySelector(".pulse");
 tiradaBt.onclick = tirada;
@@ -46,23 +46,23 @@ tiradaBt.onclick = tirada;
 let effects = document.querySelector("#effects");
 
 function tirada() {
-    display[0][1].classList.add("blur");
-    display[1][1].classList.add("blur");
-    display[2][1].classList.add("blur");
+    display[0][1].classList.remove("blur");
+    display[1][1].classList.remove("blur");
+    display[2][1].classList.remove("blur");
     
     effects.play();
 
-    let interval0 = (parseInt(Math.random() * 100) + 30);
-    let interval1 = (parseInt(Math.random() * 100) + 30);
-    let interval2 = (parseInt(Math.random() * 100) + 30);
+    interval0 = 60;
+    interval1 = interval0;
+    interval2 = interval0;
 
     timer0 = setInterval(changeImg0, interval0);
     timer1 = setInterval(changeImg1, interval1);
     timer2 = setInterval(changeImg2, interval2);
 
-    let timeout0 = (parseInt(Math.random() * 6) + 5) * 1000;
-    let timeout1 = timeout0 * 1.25;
-    let timeout2 = timeout0 * 1.5;
+    let timeout0 = 4000;
+    let timeout1 = 6000;
+    let timeout2 = 8500;
 
     let contador0 = setTimeout(freno0, timeout0);
     let contador1 = setTimeout(freno1, timeout1);
@@ -75,6 +75,9 @@ function tirada() {
 }
 
 function changeImg0() {
+    clearInterval(timer0);
+    interval0+=1;
+    timer0 = setInterval(changeImg0, interval0);
     if ((index[0][0] + 1) < 15) {
         index[0][2] = index[0][1];
         index[0][1] = index[0][0];
@@ -88,6 +91,9 @@ function changeImg0() {
     }
 }
 function changeImg1() {
+    clearInterval(timer1);
+    interval1+=8;
+    timer1 = setInterval(changeImg1, interval1);
     if ((index[1][0] + 1) < 15) {
         index[1][2] = index[1][1];
         index[1][1] = index[1][0];
@@ -101,6 +107,9 @@ function changeImg1() {
     }
 }
 function changeImg2() {
+    clearInterval(timer2);
+    interval2+=12;
+    timer2 = setInterval(changeImg2, interval2);
     if ((index[2][0] + 1) < 15) {
         index[2][2] = index[2][1];
         index[2][1] = index[2][0];
